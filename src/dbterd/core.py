92244826ea0e09387a1f6330fd440bbdec2c7027
Charlie You <charlie@castle.finance>
2023-01-09 17:22:44 -0500

    Add table descriptions

--- src/dbterd/core.py
+++ src/dbterd/core.py
@@ -28,13 +28,14 @@ def loadModel(catalog_path, schema_path):
     return catalog, schema
 
 
-def createTable(dbml_path, model):
+def createTable(dbml_path, model, schema_model):
     """Create a table in the dbml file.
 
     Args:
         dbml_path (dbml file): The file where to store the table
         model (dbt model): The dbt model to extract the table and columns from
     """
+    description = schema_model.get("description", None)
     name = model["metadata"]["name"]
     columns = list(model["columns"].keys())
     start = "{"
@@ -42,7 +43,6 @@ def createTable(dbml_path, model):
 
     dbml_path.write(f"Table {name} {start} \n")
 
-    # TODO: add descriptions
     for column_name in columns:
         column = model["columns"][column_name]
         name = column["name"]
@@ -51,6 +51,10 @@ def createTable(dbml_path, model):
         )
 
         dbml_path.write(f"{name} {dtype} \n")
+
+    if description:
+        dbml_path.write(f"Note: '{description}' \n")
+
     dbml_path.write(f"{end} \n")
 
 
@@ -93,11 +97,11 @@ def genereatedbml(schema_path, catalog_path, dbml_path):
     catalog, schema = loadModel(catalog_path, schema_path)
     model_names = catalog["nodes"]
 
-    tables = [model["name"] for model in schema["models"]]
+    tables = {model["name"]: model for model in schema["models"]}
 
     with open(dbml_path, "w") as dbml_file:
         for model_name in model_names:
             model = catalog["nodes"][model_name]
-            if model["metadata"]["name"] in tables:
-                createTable(dbml_file, model)
+            if model["metadata"]["name"] in tables.keys():
+                createTable(dbml_file, model, tables[model["metadata"]["name"]])
         createRelatonship(dbml_file, schema)
